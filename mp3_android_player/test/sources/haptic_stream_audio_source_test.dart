6ea6eb6075607fe55059b966245ef54c84b7ac68
Mark Mixson <mixson_mark@bah.com>
2026-06-17 15:07:52 -0400

    cleanup

--- mp3_android_player/test/sources/haptic_stream_audio_source_test.dart
+++ mp3_android_player/test/sources/haptic_stream_audio_source_test.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:io';
 import 'dart:typed_data';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:mp3_android_player/sources/haptic_stream_audio_source.dart';
