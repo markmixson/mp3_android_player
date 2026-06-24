6dce69f73fe5952f3b80fdb55cd8b341d37b82f7
Mark Mixson <mixson_mark@bah.com>
2026-06-24 17:56:44 -0400

    more logging

--- mp3_android_player/lib/wrappers/advanced_haptics_wrapper.dart
+++ mp3_android_player/lib/wrappers/advanced_haptics_wrapper.dart
@@ -1,12 +1,16 @@
 import 'package:advanced_haptics/advanced_haptics.dart';
+import 'package:flutter/rendering.dart';
 
 // coverage:ignore-file
 class AdvancedHapticsWrapper {
   Future<void> playWaveform(final List<int> timings, final List<int> amplitudes) async {
-    return AdvancedHaptics.playWaveform(timings, amplitudes);
+    debugPrint("playing waveForms with ${timings.length} timings and ${amplitudes.length} amplitudes");
+    final output = await AdvancedHaptics.playWaveform(timings, amplitudes);
+    debugPrint("Finished playing waveForms with ${timings.length} timings and ${amplitudes.length} amplitudes");
+    return output;
   }
 
   Future<void> stop() async {
-    return AdvancedHaptics.stop();
+    return await AdvancedHaptics.stop();
   }
 }
