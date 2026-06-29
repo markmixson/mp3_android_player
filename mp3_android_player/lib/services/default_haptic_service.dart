5cff39a7e938ac69a6da0588c5e92f42e20b85ef
Mark Mixson <mixson_mark@bah.com>
2026-06-29 15:03:15 -0400

    go back to previous haptic service

--- mp3_android_player/lib/services/default_haptic_service.dart
+++ mp3_android_player/lib/services/default_haptic_service.dart
@@ -17,8 +17,8 @@ class DefaultHapticService implements HapticService {
     if (pattern.isNotEmpty && !skip) {
       try {
         await _hapticsWrapper.playWaveform(
-          List.filled(pattern.length, sampleWindowDurationMillis),
-          pattern,
+          _generateFlattenedList(pattern.length, sampleWindowDurationMillis),
+          _prependZeros(pattern),
         );
         return pattern.length;
       } catch (e) {
@@ -26,6 +26,20 @@ class DefaultHapticService implements HapticService {
       }
     }
     return 0;
+  }
+
+  List<int> _prependZeros(final List<int> input) {
+    return input.expand((element) => [0, element]).toList();
+  }
+
+  List<int> _generateFlattenedList(
+    final int n,
+    final int sampleWindowDurationMillis,
+  ) {
+    return List<int>.generate(
+      n * 2,
+      (index) => index % 2 == 0 ? 0 : sampleWindowDurationMillis,
+    );
   }
 
   @override
--- mp3_android_player/test/services/default_haptic_service_test.dart
+++ mp3_android_player/test/services/default_haptic_service_test.dart
@@ -56,11 +56,14 @@ void main() {
             duration.inMilliseconds,
           );
 
-          final expectedTimings = List<int>.filled(pattern.length, duration.inMilliseconds);
+          final expectedTimings = List<int>.generate(
+            pattern.length * 2,
+            (index) => index % 2 == 0 ? 0 : duration.inMilliseconds,
+          );
           verify(
             () => mockHapticsWrapper.playWaveform(
               expectedTimings,
-              pattern,
+              pattern.expand((element) => [0, element]).toList(),
             ),
           ).called(1);
         } else {
